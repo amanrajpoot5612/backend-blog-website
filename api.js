b212ae943ab31b727f186957ba4d03940fa1b006
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-07-01 21:46:44 +0530

    feat: update dependencies to include bcrypt and jsonwebtoken

--- api.js
+++ api.js
@@ -1,8 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config(); // Make sure to call this BEFORE accessing process.env
 
-const FRONTEND_URI = process.env.FRONTEND_URI;
+const FRONTEND_URI = 'http://localhost:5173';
+// const FRONTEND_URI = process.env.FRONTEND_URI; you know adsfjkl;\
 const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 4000;
 
-export { FRONTEND_URI, MONGODB_URI, PORT };
+export { FRONTEND_URI, MONGODB_URI, PORT };\\\\\\\\\ads;adsfjkl\
+adsfjkl;\
