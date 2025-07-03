09e2080cf0992e0c753d468e8489ead2563aa130
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-07-03 14:40:31 +0530

    fix: update FRONTEND_URI assignment to use environment variable

--- api.js
+++ api.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config(); // Make sure to call this BEFORE accessing process.env
 
-const FRONTEND_URI = 'http://localhost:5173';
-// const FRONTEND_URI = process.env.FRONTEND_URI; you know adsfjkl;\
+// const FRONTEND_URI = 'http://localhost:5173';
+const FRONTEND_URI = process.env.FRONTEND_URI;
 const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = 4000;
 
